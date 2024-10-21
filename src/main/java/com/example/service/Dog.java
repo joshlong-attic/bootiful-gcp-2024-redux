14210492dfdc91b2a2a2a83c774d774ea027333d
Josh Long <josh@joshlong.com>
2024-10-21 18:50:29 +0300

    envrc

--- src/main/java/com/example/service/Dog.java
+++ src/main/java/com/example/service/Dog.java
@@ -4,5 +4,5 @@ import com.google.cloud.spring.data.spanner.core.mapping.Table;
 import org.springframework.data.annotation.Id;
 
 @Table(name = "dog")
-public record Dog(@Id String id, String name) {
+ public record Dog(@Id String id, String name) {
 }
